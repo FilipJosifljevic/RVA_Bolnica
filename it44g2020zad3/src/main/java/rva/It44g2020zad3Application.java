ba017c5577b1c026b8aed5db8c0cead018b1d1c3
IT44-2020 <IT44-2020@AR1-12.ftn.ns>
2023-02-28 08:51:52 +0100

    Kreiran inicijalni projekat

---
+++ it44g2020zad3/src/main/java/rva/It44g2020zad3Application.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It44g2020zad3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It44g2020zad3Application.class, args);
+	}
+
+}
---
+++ it44g2020zad3/src/test/java/rva/It44g2020zad3ApplicationTests.java
@@ -0,0 +1,13 @@
+package rva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It44g2020zad3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
